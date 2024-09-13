096cb83a99b539d07bf814c214b48cbf4b6c59d7
Fadel <fadelaulianaldi@gmail.com>
2024-09-13 14:37:08 +0700

    make switch case temperature file

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var celcius int
+	var pilihan int
+	fmt.Print("Enter temperature in celcius: ")
+	fmt.Scanln(&celcius)
+	fmt.Println("1. Fahrenheit")
+	fmt.Println("2. Kelvin")
+	fmt.Println("3. Reamur")
+	fmt.Printf("Select temperature do you want to convert to: ")
+	fmt.Scanln(&pilihan)
+
+	switch pilihan {
+	case 1:
+		fahrenheit := celcius*9/5 + 32
+		fmt.Println("Temperature in fahrenheit is: ", fahrenheit, " F")
+	case 2:
+		kelvin := celcius + 273
+		fmt.Println("Temperature in kelvin is: ", kelvin, " K")
+	case 3:
+		reamur := celcius * 4 / 5
+		fmt.Println("Temperature in reamur is: ", reamur, " R")
+	}
+}
